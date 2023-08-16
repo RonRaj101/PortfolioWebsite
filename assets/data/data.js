46eb67aefa7a8257bdaa9feb2dbfb2c558e70308
Ronit Rai <43289911+RonRaj101@users.noreply.github.com>
2023-08-16 17:51:28 +0100

    first commit

---
+++ assets/data/data.js
@@ -0,0 +1,75 @@
+export var project_data = [
+    {
+        "name": "Muhafizz",
+        "desc": "Muhafizz is a revolutionary application designed to prioritise your safety and security by keeping you up-to-date with events and incidents that may be potentially life-threatening.",
+        "images":{
+            "main": "../assets/img/Projects/Muhafizz/muhafizz login cover.jpeg",
+            "secondary": "../assets/img/Projects/Muhafizz/website-img.png"
+        },
+        "links":{
+            "1": {
+                "link_name":"Official Website",
+                "link_url": "https://ronraj101.github.io/Muhafizz/"
+            },
+            "2": {
+                "link_name":"Play Store",
+                "link_url": "https://play.google.com/store/apps/details?id=com.muhafizz.muhafizzapp"
+            },
+            "3":{
+                "link_name":"Mobile Application Prototype",
+                "link_url": "https://www.figma.com/file/EmBjUC362I2YhZe1SDg7Po/Muhafizz-Proto?type=design&node-id=0%3A1&mode=dev"
+            },
+            "4":{
+                "link_name":"Source Code",
+                "link_url": "https://github.com/RonRaj101/Muhafizz"
+            }   
+        },
+        "tools":{
+            "1": "html",
+            "2":"css",
+            "3": "js",
+            "4":"bootstrap",
+            "5": "figma",
+            "6":"flutter",
+            "7":"firebase",
+            "8":"godaddy",
+            "9":"playstore"
+        },
+        "tags":{
+            "1": "webdev",
+            "2":"appdev",
+            "3":"other"
+        }
+    },
+
+    {
+        "name": "FrameInk",
+        "desc": "FrameInk is your one-stop-shop for all your design and development needs. We serve clients across the world, providing them with our expertise in everything from game development and web/mobile app development, to cutting-edge technologies like machine learning and virtual/augmented reality.",
+        "images":{
+            "main": "../assets/img/Projects/FrameInk/frameink.webp",
+            "secondary": "../assets/img/Projects/FrameInk/website-img.png"
+        },
+        "links":{
+            "1": {
+                "link_name":"Website",
+                "link_url": "https://ronraj101.github.io/FRAMEINK/"
+            },
+            "2": {
+                "link_name":"Source Code",
+                "link_url": "https://github.com/RonRaj101/FRAMEINK"
+            }
+        },
+        "tools":{
+            "1": "html",
+            "2":"css",
+            "3": "js",
+            "4":"bootstrap",
+            "5": "js plugins",
+            "6":"php",
+            "7":"ionos"
+        },
+        "tags":{
+            "1": "webdev"
+        }
+    }
+];
