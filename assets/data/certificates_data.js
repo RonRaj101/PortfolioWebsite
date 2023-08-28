d4f84fc0aea1dc23822015c7de3c299bccbe5076
Ronit Rai <43289911+RonRaj101@users.noreply.github.com>
2023-08-28 14:02:45 +0100

    update

---
+++ assets/data/certificates_data.js
@@ -0,0 +1,64 @@
+export var certificates_data = [
+    {
+        "title": "Advanced Diploma for Software Engineering (ADSE)",
+        "provider": "Aptech",
+        "link": "../assets/misc/adse_degree.pdf",
+        "logo":"../assets/img/Courses + Certifications/aptech.png",
+        "completed": "2021",
+        "location": "Karachi, Pakistan",
+        "tags":{
+            "1": "C Programming",
+            "2":"ASP.NET",
+            "3":"C#",
+            "4":"SQL",
+            "5":"Java",
+            "6":"OOP",
+            "7":"Web Development",
+            "8":"SDLC",
+            "9":"Android + iOS Development",
+        }
+    },
+    {
+        "title": "Responsive Web Design",
+        "provider": "freeCodeCamp",
+        "link": "https://www.freecodecamp.org/certification/ronit_rai/responsive-web-design",
+        "logo":"../assets/img/Courses + Certifications/freecodecamp.svg",
+        "completed": "2022",
+        "location": "Online",
+        "tags":{
+            "1": "html",
+            "2":"css",
+            "3": "bootstrap",
+            "4": "js",
+            "5":"jquery"
+        }
+    },
+    {
+        "title": "Javascript Algorithms & Data Structures",
+        "provider": "freeCodeCamp",
+        "link": "https://www.freecodecamp.org/certification/ronit_rai/javascript-algorithms-and-data-structures",
+        "logo":"../assets/img/Courses + Certifications/freecodecamp.svg",
+        "completed": "2023",
+        "location": "Online",
+        "tags":{
+            "1": "javascript",
+            "2":"algorithms",
+            "3": "data-structures",
+            "4": "oop",
+            "5": "regex"
+        }
+    },
+    {
+        "title": "SQL (Basic)",
+        "provider": "Hackerrank",
+        "link": "https://www.hackerrank.com/certificates/d8137ce826ce",
+        "logo":"../assets/img/Courses + Certifications/HackerRank_Iconx.png",
+        "completed": "2022",
+        "location": "Online",
+        "tags":{
+            "1": "sql"
+        }
+    }
+];
+
+export default certificates_data;
---
+++ assets/data/experience_data.js
@@ -0,0 +1,54 @@
+var experience_data = [
+    {
+        "title": "Coding Instructor",
+        "company": "Code Ninjas",
+        "desc": "Working with children aged 5-15 to teach them the fundamentals of coding and programming. Teaching them how to create their own games and animations using Scratch, Python, and JavaScript.",
+        "website": "https://www.codeninjas.com/",
+        "logo":"../assets/codeninjas.png",
+        "duration": "2023 - ",
+        "location": "London, UK",
+        "tags":{
+            "1": "scratch",
+            "2":"python",
+            "3": "javascript"
+        }
+    },
+
+    {
+        "title": "Applications Engineer",
+        "company": "Muhafizz",
+        "desc": "Worked on the development of the Muhafizz Mobile Application on Flutter, using Firebase as the backend. Constructed the UI/UX of the application using Figma. Also developed the official landing page for the startup.",
+        "website": "https://muhafizz.com/",
+        "logo":"../assets/muhafizz.png",
+        "duration": "2021 - 2022",
+        "location": "Karachi, Pakistan",
+        "tags":{
+            "3": "flutter",
+            "6": "figma",
+            "7": "firebase",
+            "8": "bootstrap",
+            "9": "playstore",
+        }
+    },
+    {
+        "title": "Website Developer/Designer",
+        "company": "Upwork/Fiverr",
+        "desc": "Freelance Website Developer/Designer for clients across the world. Working with mainly WordPress and WooCommerce with an emphasis on customizability and responsiveness. Integrating various plugins and APIs to achieve the desired functionality. Custom Websites also developed from scratch using HTML, CSS, JS, Bootstrap, PHP, etc.",
+        "website": "https://www.upwork.com/freelancers/~01113a66916e3b3995",
+        "logo":"../assets/Upwork.png",
+        "duration": "2020 - ",
+        "location": "Remote",
+        "tags":{
+            "3": "woocommerce",
+            "6": "vue",
+            "7": "bootstrap",
+            "8": "php",
+            "9": "api",
+            "10": "laravel",
+            "11": "sql",
+        }
+    }
+ 
+];
+
+export default experience_data;
